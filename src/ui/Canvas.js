687215472536e5923bec7e6b9b5cc8f5a2c2612d
Jan Andersson <jan.andersson@gmail.com>
2018-08-09 23:21:04 +0200

    Initial gameboard

--- src/ui/Canvas.js
+++ src/ui/Canvas.js
@@ -10,10 +10,34 @@ class Canvas extends Component {
     super()
   }
 
+  drawGraphics() {
+    var graphics = new PIXI.Graphics()
+    graphics.lineStyle(1, 0xe0e0e0, 1)
+
+    for (let y = 0; y < 10; y++) {
+      graphics.moveTo(1, y * 50 + 1)
+      graphics.lineTo(450, y * 50 + 1)
+      for (let x = 0; x < 10; x++) {
+        graphics.moveTo(x * 50 + 1, 1)
+        graphics.lineTo(x * 50 + 1, 450)
+      }
+    }
+
+    graphics.lineStyle(1, 0, 1)
+    graphics.beginFill(0x88ee88)
+    graphics.drawRect(202, 202, 48, 48)
+    graphics.endFill()
+
+    return graphics
+  }
+
   componentDidMount() {
-    this.app = new PIXI.Application(800, 500, {
-      backgroundColor: 0x1099bb
+    this.app = new PIXI.Application(800, 452, {
+      backgroundColor: 0xffffff
     })
+
+    const graphics = this.drawGraphics()
+    this.app.stage.addChild(graphics)
 
     this.gameCanvas.appendChild(this.app.view)
     this.app.start()
