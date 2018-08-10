dfbe9e0799823db94935cbc57446a6cdd5d0fdc2
Jan Andersson <jan.andersson@gmail.com>
2018-08-10 22:29:36 +0200

    Resize window and move board

--- src/ui/Canvas.js
+++ src/ui/Canvas.js
@@ -6,31 +6,31 @@ class Canvas extends Component {
   app: PIXI.Application
   gameCanvas: HTMLDivElement
 
-  drawGraphics() {
+  drawGraphics(xMargin, yMargin) {
     var graphics = new PIXI.Graphics()
     graphics.lineStyle(1, 0xe0e0e0, 1)
 
     for (let y = 0; y < 10; y++) {
-      graphics.moveTo(1, y * 50 + 1)
-      graphics.lineTo(450, y * 50 + 1)
+      graphics.moveTo(xMargin, y * 50 + yMargin)
+      graphics.lineTo(450 + xMargin, y * 50 + yMargin)
       for (let x = 0; x < 10; x++) {
-        graphics.moveTo(x * 50 + 1, 1)
-        graphics.lineTo(x * 50 + 1, 450)
+        graphics.moveTo(x * 50 + xMargin, yMargin)
+        graphics.lineTo(x * 50 + xMargin, 450 + yMargin)
       }
     }
 
     graphics.lineStyle(1, 0, 1)
     graphics.beginFill(0x88ee88)
-    graphics.drawRect(202, 202, 48, 48)
+    graphics.drawRect(200 + xMargin, 200 + yMargin, 50, 50)
     graphics.endFill()
 
     return graphics
   }
 
-  makeSprite(renderer) {
+  makeSprite(renderer, xMargin, yMargin) {
     const graphics = new PIXI.Graphics()
     graphics.beginFill(0xff22aa)
-    graphics.drawRect(0, 0, 99, 49)
+    graphics.drawRect(0, 0, 100, 50)
     graphics.endFill()
     const texture = renderer.generateTexture(graphics)
     const sprite = new PIXI.Sprite(texture)
@@ -43,14 +43,23 @@ class Canvas extends Component {
         sprite.data = event.data
         sprite.alpha = 0.5
         sprite.dragging = true
+        sprite.previousX = sprite.x
+        sprite.previousY = sprite.y
       })
 
       .on('mouseup', () => {
         sprite.alpha = 1
         sprite.dragging = false
         sprite.data = null
-        sprite.x = Math.floor((sprite.x + 25) / 50) * 50 + 1
-        sprite.y = Math.floor(sprite.y / 50) * 50 + 26
+        const xPos = Math.floor((sprite.x - xMargin - 25) / 50)
+        const yPos = Math.floor((sprite.y - yMargin) / 50)
+        if (xPos >= 0 && yPos >= 0 && xPos < 8 && yPos < 9) {
+          sprite.x = xPos * 50 + 50 + xMargin
+          sprite.y = yPos * 50 + 25 + yMargin
+        } else {
+          sprite.x = sprite.previousX
+          sprite.y = sprite.previousY
+        }
       })
 
       .on('mouseupoutside', () => {
@@ -70,16 +79,27 @@ class Canvas extends Component {
   }
 
   componentDidMount() {
-    this.app = new PIXI.Application(800, 452, {
+    this.app = new PIXI.Application({
+      autoresize: true,
+      resolution: devicePixelRatio,
       backgroundColor: 0xffffff
     })
 
-    const graphics = this.drawGraphics()
+    this.app.renderer.resize(window.innerWidth, window.innerHeight - 100)
+
+    window.addEventListener('resize', () => {
+      this.app.renderer.resize(window.innerWidth, window.innerHeight - 100)
+    })
+
+    const xMargin = (window.innerWidth - 450) / 2
+    const yMargin = (window.innerHeight - 550) / 2
+
+    const graphics = this.drawGraphics(xMargin, yMargin)
     this.app.stage.addChild(graphics)
 
-    const sprite = this.makeSprite(this.app.renderer)
-    sprite.x = 550
-    sprite.y = 200
+    const sprite = this.makeSprite(this.app.renderer, xMargin, yMargin)
+    sprite.x = 550 + xMargin
+    sprite.y = 225 + yMargin
     this.app.stage.addChild(sprite)
     this.gameCanvas.appendChild(this.app.view)
     this.app.start()
