af293b05f511baaa79f0070a0d107139198d2740
Jan Andersson <jan.andersson@gmail.com>
2018-08-13 22:50:26 +0200

    Refactor Board
    - Don't clear unless needed
    - Combine draw operations

--- src/ui/Board.js
+++ src/ui/Board.js
@@ -4,18 +4,16 @@ import * as PIXI from 'pixi.js'
 export const behavior = {
   customDisplayObject: props => new PIXI.Graphics(),
   customApplyProps: function(instance, oldProps, newProps) {
-    instance.clear()
-    const { x, y, width, height } = newProps
+    if (oldProps !== undefined) instance.clear()
+    const { width, height } = newProps
     instance.lineStyle(1, 0xe0e0e0, 1)
     const colWidth = width / 9
     const lineHeight = height / 9
 
     for (let line = 0; line < 10; line++) {
-      instance.moveTo(x, y + line * lineHeight)
-      instance.lineTo(x + width, y + line * lineHeight)
+      instance.moveTo(0, line * lineHeight).lineTo(width, line * lineHeight)
       for (let col = 0; col < 10; col++) {
-        instance.moveTo(x + col * colWidth, y)
-        instance.lineTo(x + col * colWidth, y + height)
+        instance.moveTo(col * colWidth, 0).lineTo(col * colWidth, height)
       }
     }
   }
