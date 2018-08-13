81c5d5bd02fa5eb68b8696610f42e9796800d2f2
Jan Andersson <jan.andersson@gmail.com>
2018-08-14 01:10:20 +0200

    Merge Canvas into App

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
-import Canvas from '../Canvas'
+import PropTypes from 'prop-types'
+import { Stage, Container } from 'react-pixi-fiber'
+import Board from '../Board'
+import Domino from '../Domino'
 
 class App extends Component {
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired
+  }
+
   componentDidMount() {
     this.props.resize(window.innerWidth, window.innerHeight)
     window.addEventListener('resize', () =>
@@ -10,10 +18,28 @@ class App extends Component {
   }
 
   render() {
+    const { width, height } = this.props
+    const sideLength = (height < width ? height : width) - 40
+    const dominoLength = sideLength / 9
     return (
-      <div className="App">
-        <Canvas width={this.props.width} height={this.props.height} />
-      </div>
+      <Stage
+        options={{ backgroundColor: 0xffffff }}
+        width={width}
+        height={height}
+      >
+        <Container
+          x={width / 2 - sideLength / 2}
+          y={height / 2 - sideLength / 2}
+        >
+          <Board width={sideLength} height={sideLength} />
+          <Domino
+            width={2 * dominoLength}
+            height={dominoLength}
+            x={sideLength + 2 * dominoLength}
+            y={sideLength / 2}
+          />
+        </Container>
+      </Stage>
     )
   }
 }
