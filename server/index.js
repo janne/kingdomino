f86a4d92c73c8c37be88a6d14e2cb9d70d17d96e
Jan Andersson <jan.andersson@gmail.com>
2018-09-06 21:00:58 +0200

    Run server on port 3001

--- server/index.js
+++ server/index.js
@@ -8,6 +8,6 @@ http
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end('Hello, World!\n')
   })
-  .listen(3000, 'localhost')
+  .listen(3001, 'localhost')
 
-console.log('Server running at http://localhost:3000/')
+console.log('Server running at http://localhost:3001/')
