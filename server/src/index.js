9e8b4b8bcd21f6994f31abe399a0eda0d2bfe297
Jan Andersson <jan.andersson@gmail.com>
2018-09-06 23:03:48 +0200

    Add body-parser to receive POSTs

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import getBoard from './getBoard'
 import getPoints from './getPoints'
 import isValid from './isValid'
 
 const app = express()
+app.use(bodyParser.urlencoded())
 
-app.get('/', (req, res) => res.send('Hello, World!'))
+const api = new express.Router()
+
+app.use('/api', api)
 
 app.listen(3001, () => console.log('Server running at http://localhost:3001/'))
