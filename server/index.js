67f64356014daedd6199aa28220116a41dbfc3a9
Jan Andersson <jan.andersson@gmail.com>
2018-09-06 20:58:05 +0200

    Make babel server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
-export { default as getBoard } from './getBoard'
-export { default as getPoints } from './getPoints'
-export { default as isValid } from './isValid'
+import http from 'http'
+import getBoard from './getBoard'
+import getPoints from './getPoints'
+import isValid from './isValid'
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Hello, World!\n')
+  })
+  .listen(3000, 'localhost')
+
+console.log('Server running at http://localhost:3000/')
