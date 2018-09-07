18ff30bec05be41f1284641c6654d9d3e4350373
Jan Andersson <jan.andersson@gmail.com>
2018-09-07 15:54:20 +0200

    Randomize deck

--- server/src/commands/init.js
+++ server/src/commands/init.js
@@ -1,14 +1,31 @@
 import R from 'ramda'
 import stack from '../stack'
 
-const picked = stack.pop()
-const initialState = {
-  placements: [],
-  deck: stack,
-  picked
+let seed = new Date().getTime()
+
+const random = () => {
+  const x = Math.sin(seed++) * 10000
+  return x - Math.floor(x)
+}
+
+const shuffle = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
+}
+
+const initialState = () => {
+  const deck = shuffle(stack)
+  const picked = deck.pop()
+  return { placements: [], deck, picked }
 }
 
 export default ({ client }) => (req, res) => {
-  client.setState(initialState)
-  res.json(R.pick(['placements', 'picked'], initialState))
+  const state = initialState()
+  client.setState(state)
+  res.json(R.pick(['placements', 'picked'], state))
 }
