337c4c382a36f98ef2427531ed2afec8dd244cd7
Jan Andersson <jan.andersson@gmail.com>
2018-09-07 14:53:35 +0200

    Start with clean board

--- server/src/commands/init.js
+++ server/src/commands/init.js
@@ -1,13 +1,11 @@
 import R from 'ramda'
 import stack from '../stack'
 
+const picked = stack.pop()
 const initialState = {
-  placements: [
-    { x: 1, y: 0, dir: 1, domino: stack[1] },
-    { x: -2, y: 0, dir: 0, domino: stack[0] }
-  ],
-  deck: stack.slice(3),
-  picked: stack[4]
+  placements: [],
+  deck: stack,
+  picked
 }
 
 export default ({ client }) => (req, res) => {
