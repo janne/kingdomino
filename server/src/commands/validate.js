8bce105a2541482f95172bd6cf6685819f1af3ca
Jan Andersson <jan.andersson@gmail.com>
2018-09-07 14:52:14 +0200

    Pop new picked every turn

--- server/src/commands/validate.js
+++ server/src/commands/validate.js
@@ -9,10 +9,15 @@ export default ({ client }) => (req, res) => {
       const pos = R.map(parseInt, R.pick(['x', 'y', 'dir'], req.body))
       const newPlacements = [...placements, { ...pos, domino: picked }]
       const ok = getBoard(newPlacements) !== null
+      const newPicked = state.deck.pop()
       if (ok) {
-        client.setState({ ...state, placements: newPlacements })
+        client.setState({
+          ...state,
+          placements: newPlacements,
+          picked: newPicked
+        })
       }
-      res.send({ ok, placements: newPlacements, picked })
+      res.send({ ok, placements: newPlacements, picked: newPicked })
     })
     .catch(error => res.send({ ok: false, error }))
 }
